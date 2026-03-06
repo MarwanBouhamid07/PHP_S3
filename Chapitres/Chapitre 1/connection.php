a31e0a0cd1716da5aef8b3d7c15cfeff6191e22d
Marwan bouhamid <marwanbouhamid123@gmail.com>
2026-03-06 10:55:51 +0100

    Chapitre 1

---
+++ Chapitres/Chapitre 1/connection.php
@@ -0,0 +1,15 @@
+<?php
+$host = '127.0.0.1';
+$dbname = 'store_db';
+$username = 'root';
+$password = '';
+
+$dsn= "mysql:host=$host;dbname=$dbname;charset=utf8";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connection is succese with database : $dbname";
+} catch (PDOException $e) {
+    echo "have an error in connection :" . $e->getMessage();
+}
