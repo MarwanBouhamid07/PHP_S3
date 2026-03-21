d1f60a2a278cc6e58de42e98a8fe04523e3f82e3
Marwan bouhamid <marwanbouhamid123@gmail.com>
2026-03-21 21:40:46 +0100

    Chapitre 8

--- Chapitres/Object Oriented Programming/Chapitre 8/test.php
+++ Chapitres/Object Oriented Programming/Chapitre 8/test.php
@@ -2,6 +2,6 @@
 
 require_once 'BlogArticle.php';
 
-$article = new BlogArticle("POO in PHP","Decouvrir l'heritage.","Marwan");
+$article = new BlogArticle("POO in PHP","Decouvrir l'heritage.","Ahmed");
 
 echo $article->display();
