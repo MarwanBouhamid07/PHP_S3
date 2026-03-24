7ea830d903b163916c2c871bee3d5d2e6528c161
Marwan bouhamid <marwanbouhamid123@gmail.com>
2026-03-24 20:56:22 +0100

    prototype

---
+++ Prototype/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/8f8b4a4f39.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <div class="logo-blog">MARWAN BLOG</div>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Blog</a>
+            <a href="">Categories</a>
+            <a href="">Contact</a>
+        </nav>
+    </header>
+<div class="wrapper">
+    <main>
+        <section>
+            <img src="" alt="">
+            <p class="date">March 15-2024</p>
+            <p class="autor">Ahmed</p>
+            <h3 class="title-article">Blog Post title Goes here</h3>
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis pariatur repellendus nihil ab reiciendis iusto culpa vel accusamus tempore consectetur officiis ea, natus iste! Expedita laboriosam libero natus veritatis ullam.</p>
+            <button class="more">Read More</button>
+        </section>
+        <section>
+            <img src="" alt="">
+            <p class="date">March 15-2024</p>
+            <p class="autor">Ahmed</p>
+            <h3 class="title-article">Blog Post title Goes here</h3>
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis pariatur repellendus nihil ab reiciendis iusto culpa vel accusamus tempore consectetur officiis ea, natus iste! Expedita laboriosam libero natus veritatis ullam.</p>
+            <button class="more">Read More</button>
+        </section>
+        <section>
+            <img src="" alt="">
+            <p class="date">March 15-2024</p>
+            <p class="autor">Ahmed</p>
+            <h3 class="title-article">Blog Post title Goes here</h3>
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis pariatur repellendus nihil ab reiciendis iusto culpa vel accusamus tempore consectetur officiis ea, natus iste! Expedita laboriosam libero natus veritatis ullam.</p>
+            <button class="more">Read More</button>
+        </section>
+        <div class="numbering">
+            <a href="">1</a>
+            <a href="">2</a>
+            <a href="">3</a>
+        </div>
+    </main>
+    <aside>
+        <div class="search">
+            <form method="GET">
+                <label for="search">Search:</label>
+                <input type="text" placeholder="search..." name="search" id="search">
+                <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+
+        <div class="categories">
+            <div class="categorie">
+                <a href="">Technology</a>
+                <a href="">Design</a>
+                <a href="">Lifestyle</a>
+                <a href="">Travel</a>
+                <a href="">News</a>
+            </div>
+        </div>
+            <div class="recent-posts">
+                <div class="lasts-post">
+                    <img src="" alt="">
+                    <div class="title-and-date">
+                        <div class="title-RP">Post Title Here</div>
+                        <div class="date-RP">Mar 15</div>
+                    </div>
+                </div>
+                <div class="lasts-post">
+                    <img src="" alt="">
+                    <div class="title-and-date">
+                        <div class="title-RP">Post Title Here</div>
+                        <div class="date-RP">Mar 15</div>
+                    </div>
+                </div>
+                <div class="lasts-post">
+                    <img src="" alt="">
+                    <div class="title-and-date">
+                        <div class="title-RP">Post Title Here</div>
+                        <div class="date-RP">Mar 15</div>
+                    </div>
+                </div>
+            </div>
+            <div class="tags">
+                <a href="">Design</a>
+                <a href="">Tech</a>
+                <a href="">Web</a>
+                <a href="">UI/UX</a>
+                <a href="">Code</a>
+            </div>
+    </aside>
+</div>
+<footer>
+    <div class="head-footer">
+        <div class="about">
+            <h2 class="title-foot">About</h2>
+            <p class="description-foot">Brief description about the blog and its purpose goes here.</p>
+        </div>
+        <div class="links">
+            <h2 class="title-foot">Quick links</h2>
+            <p class="description-foot">
+                <a href="">Privacy Policy</a>
+                <a href="">Terms of Service</a>
+                <a href="">Contact Us</a>
+            </p>
+        </div>
+        <div class="social-media">
+            <h1 class="title-foot">Follow us</h1>
+            <p>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </p>
+        </div>
+    </div>
+    <div class="foot-footer">&copy; 2026 Blog Name. All rights reserved </div>
+</footer>
+</body>
+</html>
